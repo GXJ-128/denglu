5378d58d2534513bdbcd4a6bcde6853e3a567a29
GXJ-128 <15001292477@163.com>
2018-05-30 23:13:36 +0800

    123123

--- test.php
+++ test.php
@@ -1,10 +1,40 @@
 <?php
-	$User=$_GET['name'].$_GET['pass'];
-	$MyUser='zhangsan';
-	$MyPass='123123';
-	if($User==$MyUser.$MyPass){
-		echo 'ok';
-	}else{
-		echo 'error';
+	$user=$_POST['id'].$_POST['name'].$_POST['pass'];
+	$lists=array(
+		array(
+			'Id'=>'1',
+			'username'=>'zhangsan',
+			'password'=>'123123'
+		),
+		array(
+			'Id'=>'2',
+			'username'=>'lisi',
+			'password'=>'123456'
+		),
+		array(
+			'Id'=>'3',
+			'username'=>'zhangsan',
+			'password'=>'120102'
+		),
+		array(
+			'Id'=>'4',
+			'username'=>'lisi',
+			'password'=>'123456'
+		),
+		array(
+			'Id'=>'5',
+			'username'=>'zhangsan',
+			'password'=>'120102'
+		),
+	);
+	foreach($lists as $key=>$val){ 
+//	echo $key."<hr>";
+		if($user==$val["Id"].$val["username"].$val["password"]){
+			echo 'ok';
+			return;
+		}else{
+			echo 'error';
+			return;
+			}
 	}
 ?>
